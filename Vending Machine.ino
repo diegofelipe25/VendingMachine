14eb936c3f8f7663a821113dde0a07205c3e1fd8
diegofelipe25 <diego_felipe25@outlook.com>
2022-12-13 11:17:59 -0300

    auditoria feita por josma

--- Vending Machine.ino
+++ Vending Machine.ino
@@ -1,9 +1,9 @@
 #include <Arduino.h>
 const int numero_motores=40; //quantidade de motores
-const int numero_sensores=6; // quantidade de sensores
+const int numero_sensores=2; // quantidade de sensores
 int motores[numero_motores] = {8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,
 33,34,35,36,37,38,39,40,40,42,43,44,45,46,47}; //portas que serao usadas pelos motores, 40
-int sensores[numero_sensores] = {2,3,4,5,6,7}; //portas que serao usadas pelos sensores, 6
+int sensores[numero_sensores] = {2,3}; //portas que serao usadas pelos sensores, 2
 int sensores_status_leitura=1;
 int NUMERO_MOTOR=0;
 int ESTADO_SENSOR=0;
@@ -29,7 +29,7 @@ void setup(){
 Serial.begin(9600);
 // pinos sensores
 for (int i=2;i<=numero_sensores+1;i++){
-  pinMode(i, INPUT); // pino 2 ao 7 serao sensores, 6 sensores 
+  pinMode(i, INPUT); // pino 2 ao 4 serao sensores, 2 sensores 
 }
 // pinos motores 
 for (int i=8;i<=numero_motores+7;i++){
@@ -53,9 +53,10 @@ void loop(){
    }while(true);
    if(NUMERO_MOTOR >= motores[0] and NUMERO_MOTOR <= numero_motores+7){
       AcionaMotor(NUMERO_MOTOR,HIGH);
+    
       do{  
        for(int i=0; i<numero_sensores; i++) { 
-        sensores_status_leitura=digitalRead(i);
+        //sensores_status_leitura=digitalRead(i);
         // Serial.println(i);   
         // Serial.println(sensores_status_leitura); 
         Serial.println("Girando o motor");
@@ -66,6 +67,7 @@ void loop(){
          }
        }
      }while(ESTADO_SENSOR==0);    
+   
      if(ESTADO_SENSOR==1){
       AcionaMotor(NUMERO_MOTOR,LOW); 
       iniciaTudo();
