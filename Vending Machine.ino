eceb380380ba1df7ddbab65f76759bdd20d8c949
diegofelipe25 <diego_felipe25@outlook.com>
2022-12-15 08:29:12 -0300

    adicao de mais comentarios

--- Vending Machine.ino
+++ Vending Machine.ino
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+//@autor diego felipe
 const int numero_motores=40; //quantidade de motores
 const int numero_sensores=2; // quantidade de sensores
 int motores[numero_motores] = {8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,
@@ -28,7 +29,7 @@ void iniciaTudo(){ //zera as variaveiveis antes de dar um novo loop
 void setup(){
 Serial.begin(9600);
 // pinos sensores 
-//sensor manda sinal 1 continuo, ou seja 5V  se detectar algo mando 0v ou seja sinal 1 ate o objeto sair da frente do sensor
+//sensor manda sinal 1 continuo, ou seja 5V  se detectar algo mando 0v ou seja sinal 0 ate o objeto sair da frente do sensor
 for (int i=2;i<=numero_sensores+1;i++){
   pinMode(i, INPUT); // pino 2 ao 4 serao sensores, 2 sensores 
 }
@@ -41,16 +42,16 @@ for (int i=8;i<=numero_motores+7;i++){
 
 void loop(){   
    for(int i=0; i<numero_sensores; i++) { //le os sensores a primeira vez antes de pedir o numero do motor
-    sensores[i]=digitalRead(i+2);
+    sensores[i]=digitalRead(i+2); //passa o valor da porta digital para o indice da lista de sensores
   }
    Serial.println("\n");
    Serial.println("Digite a porta do motor: ");
    delay(1500); 
    do{
      if(Serial.available()){ //se tiver alguma coisa na serial entra aqui
-       NUMERO_MOTOR = Serial.parseInt();
+       NUMERO_MOTOR = Serial.parseInt(); //converte o valor recebido para inteiro e passa para a variavel
        Serial.read(); //aqui é para nao pegar o ENTER 
-       break;
+       break; //quebra o while
      }
    }while(true);
    if(NUMERO_MOTOR >= motores[0] and NUMERO_MOTOR <= numero_motores+7){ //verifica se o motor esta dentro da lista de motores
@@ -73,7 +74,7 @@ void loop(){
      if(ESTADO_SENSOR==1){ //se detectar algo vai entrar aqui e vai parar o motor
       AcionaMotor(NUMERO_MOTOR,LOW); //chama o metodo de acionar motor e desliga
       iniciaTudo(); //zera as variaveis para o estado inicial
-      delay(500);
+      delay(500); //espera meio segundo que é o tempo para resetar as variaveis
       Serial.flush(); //limpa o cache da serial
       }
    }else{
